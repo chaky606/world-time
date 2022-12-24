5d3c2c86d0333ab95143bac65019565940f84d1f
chaky606 <koychak@gmail.com>
2022-12-24 15:02:41 +0700

    Flutter Package (http) + Asyn function + JsonPlaceholder

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,8 +11,8 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void getData() async{  // <-- used for waiting response processing
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-    Map data = jsonDecode(response.body);
+    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));  // <-- get API address from http
+    Map data = jsonDecode(response.body);  // <-- get API properties to List by convert to string by use jsonDecode
     print(data['title']);
   }
 
@@ -22,7 +22,7 @@ class _LoadingState extends State<Loading> {
     getData();
   }
 
-  @override  // <-- body of Statefull
+  @override  // <-- body of StateFull
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Text('Loading Screen'),
